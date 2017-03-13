eadc548be758af3061f86d34976d53b7ff6e66ae
SU JIN (Emily) CHO <chosujin84@gmail.com>
2017-03-12 19:10:19 -0700

    For security...

--- oauth_stream.py
+++ oauth_stream.py
@@ -7,11 +7,6 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 from tweepy import Stream
 
-consumer_key="YZ0djZAoGRUVQx8heiaOr5nNG"
-consumer_secret="wrltfnZbABhZss1nlsppIBvAOm4CSim4IkbaFFRmFgVpIgQaky"
-access_token="838107760721985536-L2rzrPWAfN5PI1gITEF8NifSSeK0QjE"
-access_token_secret="Zopx5zi0o3tKV0GviXdx4Nsb7aJJkDN7XvRvFc7DD3KMK"
-
 class MyStreamListener(tweepy.StreamListener):
 	"""A listener handles tweets that are received from the stream.
 	This is a basic listener that just prints received tweets to stdout
